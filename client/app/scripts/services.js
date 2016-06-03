e48468fcc6c83136a631ffae3406befabe226cf1
y <y@localhost.localdomain>
2016-06-03 13:56:56 -0400

    updated resource

--- client/app/scripts/services.js
+++ client/app/scripts/services.js
@@ -4,7 +4,7 @@ angular.module('project1App')
 .constant("baseURL", "http://localhost:3001/")
 .factory('phoneFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-        return $resource(baseURL + "phones/", null, {
+        return $resource(baseURL + "phones/:id", null, {
             'update': {
                 method: 'PUT'
             }
